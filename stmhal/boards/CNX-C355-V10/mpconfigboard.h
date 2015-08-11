890f29b1ace9c93cb23bf10f537371bc9623682a
Damien George <damien.p.george@gmail.com>
2015-08-11 14:48:27 +0100

    stmhal: Add support for Connexi C355.

---
+++ stmhal/boards/CNX-C355-V10/mpconfigboard.h
@@ -0,0 +1,68 @@
+#define MICROPY_HW_BOARD_NAME       "CNX-C355-V10"
+#define MICROPY_HW_MCU_NAME         "STM32F405RG"
+#define MICROPY_PY_SYS_PLATFORM     "pyboard"
+
+#define MICROPY_HW_HAS_SWITCH       (1)
+#define MICROPY_HW_HAS_SDCARD       (0)
+#define MICROPY_HW_HAS_MMA7660      (0)
+#define MICROPY_HW_HAS_LIS3DSH      (0)
+#define MICROPY_HW_HAS_LCD          (0)
+#define MICROPY_HW_ENABLE_RNG       (0)
+#define MICROPY_HW_ENABLE_RTC       (1)
+#define MICROPY_HW_ENABLE_TIMER     (1)
+#define MICROPY_HW_ENABLE_SERVO     (1)
+#define MICROPY_HW_ENABLE_DAC       (0)
+#define MICROPY_HW_ENABLE_SPI1      (1)
+#define MICROPY_HW_ENABLE_SPI2      (0)
+#define MICROPY_HW_ENABLE_SPI3      (1)
+#define MICROPY_HW_ENABLE_CAN       (0)
+
+// HSE is 12MHz
+#define MICROPY_HW_CLK_PLLM (7)
+#define MICROPY_HW_CLK_PLLN (196)
+#define MICROPY_HW_CLK_PLLP (RCC_PLLP_DIV2)
+#define MICROPY_HW_CLK_PLLQ (7)
+
+// does have a 32kHz crystal
+#define MICROPY_HW_RTC_USE_LSE (1)
+
+// UART config TODO check
+#define MICROPY_HW_UART1_PORT (GPIOB)
+#define MICROPY_HW_UART1_PINS (GPIO_PIN_6 | GPIO_PIN_7)
+#define MICROPY_HW_UART2_PORT (GPIOA)
+#define MICROPY_HW_UART2_PINS (GPIO_PIN_2 | GPIO_PIN_3)
+#define MICROPY_HW_UART2_RTS  (GPIO_PIN_1)
+#define MICROPY_HW_UART2_CTS  (GPIO_PIN_0)
+#define MICROPY_HW_UART6_PORT (GPIOC)
+#define MICROPY_HW_UART6_PINS (GPIO_PIN_6 | GPIO_PIN_7)
+
+// TODO
+#define MICROPY_HW_I2C1_SCL (pin_B6)
+#define MICROPY_HW_I2C1_SDA (pin_B7)
+
+// TODO
+#define MICROPY_HW_I2C3_SCL (pin_A8)
+#define MICROPY_HW_I2C3_SDA (pin_B8)
+
+// USRSW has no pullup or pulldown; it is active low and broken out on a header
+#define MICROPY_HW_USRSW_PIN        (pin_C13)
+#define MICROPY_HW_USRSW_PULL       (GPIO_PULLUP)
+#define MICROPY_HW_USRSW_EXTI_MODE  (GPIO_MODE_IT_FALLING)
+#define MICROPY_HW_USRSW_PRESSED    (0)
+
+// Connexi has 3 LEDs
+#define MICROPY_HW_LED1             (pin_A13) // red
+#define MICROPY_HW_LED2             (pin_A14) // green
+#define MICROPY_HW_LED3             (pin_B2)  // blue
+#define MICROPY_HW_LED4             (pin_B2)  // blue
+#define MICROPY_HW_LED_OTYPE        (GPIO_MODE_OUTPUT_PP)
+#define MICROPY_HW_LED_ON(pin)      (pin->gpio->BSRRL = pin->pin_mask)
+#define MICROPY_HW_LED_OFF(pin)     (pin->gpio->BSRRH = pin->pin_mask)
+
+// SD card detect switch
+#define MICROPY_HW_SDCARD_DETECT_PIN        (pin_A10)
+#define MICROPY_HW_SDCARD_DETECT_PULL       (GPIO_PULLUP)
+#define MICROPY_HW_SDCARD_DETECT_PRESENT    (GPIO_PIN_RESET)
+
+// USB config
+#define MICROPY_HW_USB_VBUS_DETECT_PIN (pin_A9)
