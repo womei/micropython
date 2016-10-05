a5a2b69673f446aa95af7acf46b1a3216337c794
Damien George <damien.p.george@gmail.com>
2016-10-05 15:54:41 +1100

    stmhal: For CNX-C353-V10, enable HW_HAS_FLASH, remove LED4.

--- stmhal/boards/CNX-C353-V10/mpconfigboard.h
+++ stmhal/boards/CNX-C353-V10/mpconfigboard.h
@@ -3,6 +3,7 @@
 #define MICROPY_PY_SYS_PLATFORM     "pyboard"
 
 #define MICROPY_HW_HAS_SWITCH       (1)
+#define MICROPY_HW_HAS_FLASH        (1)
 #define MICROPY_HW_HAS_SDCARD       (1)
 #define MICROPY_HW_HAS_MMA7660      (0)
 #define MICROPY_HW_HAS_LIS3DSH      (0)
@@ -54,7 +55,6 @@
 #define MICROPY_HW_LED1             (pin_A13) // red
 #define MICROPY_HW_LED2             (pin_A14) // green
 #define MICROPY_HW_LED3             (pin_B2)  // blue
-#define MICROPY_HW_LED4             (pin_B2)  // blue
 #define MICROPY_HW_LED_OTYPE        (GPIO_MODE_OUTPUT_PP)
 #define MICROPY_HW_LED_ON(pin)      (pin->gpio->BSRRL = pin->pin_mask)
 #define MICROPY_HW_LED_OFF(pin)     (pin->gpio->BSRRH = pin->pin_mask)
