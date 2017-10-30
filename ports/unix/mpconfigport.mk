d4596394612dd362a6b84f1ad8b22d5ba0538623
Damien George <damien.p.george@gmail.com>
2017-10-30 15:49:37 +1100

    unix: Switch to mbedtls implementation for ussl module.

--- ports/unix/mpconfigport.mk
+++ ports/unix/mpconfigport.mk
@@ -28,7 +28,7 @@ MICROPY_PY_USSL = 1
 # axTLS has minimal size and fully integrated with MicroPython, but
 # implements only a subset of modern TLS functionality, so may have
 # problems with some servers.
-MICROPY_SSL_AXTLS = 1
+MICROPY_SSL_AXTLS = 0
 # mbedTLS is more up to date and complete implementation, but also
 # more bloated. Configuring and building of mbedTLS should be done
 # outside of MicroPython, it can just link with mbedTLS library.
